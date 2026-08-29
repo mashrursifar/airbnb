fd204510d6606608f8dc1e56ef68bb2a98d4e39c
mashrursifar <mashrursifar@gmail.com>
2026-08-29 21:30:31 +0600

    model pushed

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1550345283-f456c85af8d1?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        //setting defult image. If user doesn't share any photo
+        set: (v) =>
+            v === ""
+                ? "https://images.unsplash.com/photo-1550345283-f456c85af8d1?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
