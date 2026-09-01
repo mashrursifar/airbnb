ea3ab2c0fc1cf8fe0e0fe64a6dbd3273cc697681
mashrursifar <mashrursifar@gmail.com>
2026-09-01 14:08:09 +0600

    Nav bar and footer added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
+app.use(express.static("public"))
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
