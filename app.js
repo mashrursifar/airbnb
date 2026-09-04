7b1abafdb56f5f98c6eece90f3923ac0bd704bb2
mashrursifar <mashrursifar@gmail.com>
2026-09-04 22:32:12 +0600

    Custom error page created, and error is handled through the custom error handler

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -50,11 +51,14 @@ app.get("/", (req, res) => {
 // });
 
 // All the Listings
-app.get("/listing", async (req, res) => {
-    const allListings = await Listing.find({});
+app.get(
+    "/listing",
+    wrapAsync(async (req, res) => {
+        const allListings = await Listing.find({});
 
-    res.render("listing/listing.ejs", { allListings });
-});
+        res.render("listing/listing.ejs", { allListings });
+    }),
+);
 
 // createe new page
 app.get("/listing/new", (req, res) => {
@@ -62,50 +66,79 @@ app.get("/listing/new", (req, res) => {
 });
 
 // Create new route
-app.get("/listing/:id", async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    // console.log(listing);
-    res.render("listing/show.ejs", { listing });
-});
+app.get(
+    "/listing/:id",
+    wrapAsync(async (req, res) => {
+        let { id } = req.params;
+        const listing = await Listing.findById(id);
+        // console.log(listing);
+
+        if (!listing) {
+            throw new ExpressError(404, "Listing Not Found");
+        }
+        res.render("listing/show.ejs", { listing });
+    }),
+);
 
 // New Listing
-app.post("/listing", async (req, res) => {
-    const list = new Listing(req.body);
-    list.save();
+app.post(
+    "/listing",
+    wrapAsync(async (req, res) => {
+        const list = new Listing(req.body);
+        list.save();
 
-    res.redirect("listing");
-});
+        res.redirect("listing");
+    }),
+);
 
 // Edit form
-app.get("/listing/:id/edit", async (req, res) => {
-    let { id } = req.params;
+app.get(
+    "/listing/:id/edit",
+    wrapAsync(async (req, res) => {
+        let { id } = req.params;
 
-    let listing = await Listing.findById(id);
+        let listing = await Listing.findById(id);
 
-    res.render("listing/edit.ejs", { listing });
-});
+        res.render("listing/edit.ejs", { listing });
+    }),
+);
 
 // Edit in DB
-app.put("/listing/:id", async (req, res) => {
-    let { id } = req.params;
-    let listing = req.body;
-    console.log(id);
-    console.log(listing);
+app.put(
+    "/listing/:id",
+    wrapAsync(async (req, res) => {
+        let { id } = req.params;
+        let listing = req.body;
+        console.log(id);
+        console.log(listing);
 
-    const newListing = await Listing.findByIdAndUpdate(id, listing);
-    res.redirect(`/listing/${id}`);
-});
+        const newListing = await Listing.findByIdAndUpdate(id, listing);
+        res.redirect(`/listing/${id}`);
+    }),
+);
 
 // Destroy & delete from DB
-app.delete("/listing/:id", async (req, res) => {
-    let { id } = req.params;
+app.delete(
+    "/listing/:id",
+    wrapAsync(async (req, res) => {
+        let { id } = req.params;
 
-    let delData = await Listing.findByIdAndDelete(id);
-    console.log(delData);
-    res.redirect("/listing");
+        let delData = await Listing.findByIdAndDelete(id);
+
+        if (!delData) {
+            throw new ExpressError(404, "Listing Not Found");
+        }
+        console.log(delData);
+        res.redirect("/listing");
+    }),
+);
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = "Something went wrong" } = err;
+
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("listing/error.ejs", {
+        statusCode,
+        message
+    });
 });
-
-app.use("*", (req, res)=>{
-    
-})
