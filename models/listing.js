9d47ec19fe0a847b52c30afa8a85c402a054fc5d
mashrursifar <mashrursifar@gmail.com>
2026-09-07 20:27:37 +0600

    new schema review created

--- models/listing.js
+++ models/listing.js
@@ -1,3 +1,4 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -26,6 +27,10 @@ const listingSchema = new Schema({
     },
     location: String,
     country: String,
+    review: {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+    },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
---
+++ models/review.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    name: String,
+    rating: Number,
+    comment: String,
+});
+
+const Review = new mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
