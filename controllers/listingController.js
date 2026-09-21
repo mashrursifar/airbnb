057772012cc8adc62918673078ac3a6e439d2919
mashrursifar <mashrursifar@gmail.com>
2026-09-21 21:20:24 +0600

    Location's coordinates added in the database

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,5 +1,8 @@
 const Listing = require("../models/listing");
 const ExpressError = require("../utils/ExpressError.js");
+const mbxGeocode = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocode({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -35,15 +38,24 @@ module.exports.showListing = async (req, res) => {
 module.exports.createNewListing = async (req, res) => {
     let filename = req.file.filename;
     let url = req.file.path;
+    let location = req.body.location + ", " + req.body.country;
+
+    let response = await geocodingClient
+        .forwardGeocode({
+            query: location,
+            limit: 1,
+        })
+        .send();
 
     const newListing = {
         ...req.body,
         owner: req.user._id,
         image: { filename, url },
+        geometry: response.body.features[0].geometry,
     };
     // console.log(newListing);
     const list = new Listing(newListing);
-
+    // console.log(list);
     await list.save();
     req.flash("success", "New listing created!!");
     res.redirect("/listing");
@@ -63,20 +75,18 @@ module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
     let listing = req.body;
     // console.log(id);
-    
+
     console.log("Listing in the update route edit", listing);
     if (req.file) {
-        
         let url = req.file.path;
         let filename = req.file.filename;
         listing.image = { filename, url };
-        
     }
     console.log("Before ", listing);
     const newListing = await Listing.findByIdAndUpdate(id, listing);
     console.log("After ", newListing);
     // await newListing.save();
-    
+
     req.flash("success", "Listing edited successfully!!");
     res.redirect(`/listing/${id}`);
 };
