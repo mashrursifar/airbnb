ad56ef675f67a0ba4b71295993e96cc535ac3829
mashrursifar <mashrursifar@gmail.com>
2026-09-22 17:55:00 +0600

    Now map can show exact location in the map and also show the point

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,12 +1,12 @@
 // let mapToken = mapToken;
-console.log("Map token: ",mapToken);
+console.log("Map token: ",coordinates);
 const map = new mapboxgl.Map({
     accessToken: mapToken,
     container: "map",
     style: "mapbox://styles/mapbox/streets-v12", // Use the standard style for the map
     projection: "globe", // display the map as a globe
     zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
-    center: [90.4125, 23.8103], // center the map on this longitude and latitude
+    center: coordinates, // center the map on this longitude and latitude
 });
 
 map.addControl(new mapboxgl.NavigationControl());
@@ -15,3 +15,15 @@ map.scrollZoom.disable();
 map.on("style.load", () => {
     map.setFog({}); // Set the default atmosphere style
 });
+
+const marker = new mapboxgl.Marker({
+    color: '#00f00', // set marker color
+    scale: .7        // scale the marker size
+  })
+  .setLngLat(coordinates)
+  .addTo(map);
+
+  const popup = new mapboxgl.Popup()
+  .setHTML('<p>Exact location will be provide after booking</p>');
+
+marker.setPopup(popup);
