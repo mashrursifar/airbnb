9abae6931d1f3d2c5c70e4e836150cf9e815787a
mashrursifar <mashrursifar@gmail.com>
2026-09-24 01:44:31 +0600

    file name fixed

--- models/review.js
+++ models/review.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const User = require("./user");
+const User = require("./user.js");
 
 const reviewSchema = new Schema(
     {
